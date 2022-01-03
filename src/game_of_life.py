b249c8fda01198cff77d517d4c7360ff2f27d2af
Anestis Dimou <anestisdm77@gmail.com>
2022-01-03 13:58:29 +0200

    Comments Updates

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,24 +1,22 @@
 """Conway\'s Game of Life."""
 
-#---------------------- Askisi 1 ------------------------
 def board(n):
-    """Kataskeuastis board (pinaka paixnidiou).
+    """Constructs the board of the game.
 
-    n -- parametros diastasis pinaka
+    n -- dimension parameter of the board
 
-    Kataskeuazei anaparastasi pinaka paixnidiou me n x n kelia, opou
-    kanena keli den einai zwntano.
+    Constructs representation of the game board with n x n cells,
+    where no cell is alive
 
-    Epistrefei apaparastasi pinaka paixnidiou, h opoia einai
-    ena le3iko (dict) me n*n stoixeia.
-    Ka8e keli antistoixei se ena stoixeio me key to tuple (i,j), opou
-    i o ari8mos grammis kai j o ari8mos stilis pou brisketai to keli. 
-    (H ari8misi grammwn kai sthlwn einai apo 0 ews n-1. To panw aristera keli
-    brisketai sto (0,0).)
-    H timi (value) tou stoixeiou einai True 'h False analoga 
-    ean to keli einai zwntano 'h oxi.
+    Return representation of the game board, where it is a dictionary
+    with n x n elements.
+    Εach cell corresponds to one element with key the tuple (i, j),
+    where inumber of row and j number of column where the cell is located.
+    The value of the element is True or False depending on whether
+    the cell is alive or not
 
-    Paradeigmata:
+
+   Examples:
 
     >>> game = board(3)
     >>> len(game)
@@ -28,7 +26,7 @@ def board(n):
     >>> game[2,1]
     False
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     i=0
     ls=[]
     while i<=n-1:
@@ -41,37 +39,36 @@ def board(n):
 
 
 
-#---------------------- Askisi 2 ------------------------
 def is_alive(board, p):
-    """Elegxei ean ena keli einai zwntano.
+    """Checks whether the cell is alive.
 
-    board -- o pinakas paixnidiou opou brisketai to keli
-    p -- h 8esh tou keliou.
+    board -- game board where the cell is located
+    p -- the position of the cell.
 
-    To orisma p einai tuple tis morfis (i,j).
-    Epistrefei True ean to keli einai zwntano, alliws False.
+    The argument p is tuple of the format (i,j).
+    Returns True if the cell is alive or False.
 
-    Paradeigma:
+    Examples:
 
     >>> is_alive(board(4), (3,2))
     False
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     return board[p]
 
 
 
 def set_alive(board, p, alive):
-    """Topo8etei 'h afairei zwi apo ena keli.
+    """Places or removes live from a cell.
 
-    board -- o pinakas paixnidiou opou brisketai to keli
-    p -- h 8esh tou keliou (tuple tis morfis (i,j))
-    alive -- logiki timi.
+    board -- game board where the cell is located
+    p -- the position of the cell.
+    alive -- boolean value.
 
-    To keli ginetai zwntano ean h alive einai True. An h alive einai False,
-    to keli pe8ainei.
+    The cell becomes alive if the arg alive is True. If arg alive is False,
+    the cell die.
 
-    Paradeigmata:
+    Examples:
 
     >>> game = board(4)
     >>> is_alive(game, (3,2))
@@ -83,7 +80,7 @@ def set_alive(board, p, alive):
     >>> is_alive(game, (3,2))
     False
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     if alive==True :
         board[p]=True
     elif alive==False:
@@ -92,33 +89,33 @@ def set_alive(board, p, alive):
 
     
 def get_size(board):
-    """Mege8os pinaka paixnidiou.
+    """Size of the game board.
 
-    board -- pinakas paixnidiou.
+    board -- game board.
 
-    Epistrefei n ean to board anaparista pinaka paixnidiou n x n.
+    Returns n whether the board represents a game board n x n.
 
-    Paradeigmata:
+    Examples:
 
     >>> get_size(board(4))
     4
     >>> get_size(board(10))
     10
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     from math import sqrt
     return int(sqrt(len(board)))
 
 
-#---------------------- Askisi 3 ------------------------
+
 def copy_board(board):
-    """Antigrafo pinaka paixnidiou.
+    """Copy of the game board.
 
-    board -- pinakas paixnidiou.
+    board -- game board.
 
-    Epistrefei ena neo pinaka paixnidiou pou einai antigrafo tou board.
+    Returns a new game board whic is copy of the game board
 
-    Paradeigmata:
+    Examples:
 
     >>> game = board(10)
     >>> set_alive(game, (4,7), True)
@@ -128,23 +125,23 @@ def copy_board(board):
     >>> is_alive(game2, (4,7))
     True
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     return {x:y for x,y in board.items()}
 
 
-#---------------------- Askisi 4 ------------------------
+
 def get_iterator(board):
-    """Iterator gia sarwsi stoixeiwn pinaka paixnidiou.
+    """Iterator for parsing elements of the game board.
 
-    board -- pinakas paixnidiou.
+    board -- game board.
 
-    Epistrefei iterator pou dinei ta kelia tou board arxizontas apo 
-    ta kelia tis grammis 0: (0,0), (0,1), (0,2),..., meta akolou8oun ta 
-    kelia tis grammis 1: (1,0), (1,1), (1,2),... ktl. mexri na e3antli8oun
-    ola ta kelia. Gia ka8e keli, o iterator epistrefei tin 8esi tou kai
-    logiki timi True 'h False analogws ean einai zwntano 'h oxi.
+    Returns iterator which gives the board cells starting from the cells
+    of the row 0: (0,0), (0,1), (0,2),..., after following the cells of
+    the row 1: (1,0), (1,1), (1,2),... etc. until all the cells are finished.
+    For every cell,iterator returns the position and the boolean value
+    True or False depending on whether is alive or not.
 
-    Paradeigma:
+    Examples:
 
     >>> game = board(3)
     >>> set_alive(game, (2, 1), True)
@@ -161,25 +158,23 @@ def get_iterator(board):
     ((2, 1), True)
     ((2, 2), False)
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     ls=[]
     for x,y in board.items():
         ls.append((x,y))
     return ls
 
 
-#---------------------- Askisi 5 ------------------------
 def print_board(board):
-    """Emfanizei pinaka paixnidiou.
+    """Prints the game board.
 
-    board -- pinakas paixnidiou.
+    board -- game board.
 
-    Emfanizei ton pinaka paixnidiou board opou me mauro tetragwno 
-    (xaraktiras unicode 11035) dinontai ta zwntana kelia, kai
-    me aspro (xaraktiras unicode 11036) ta pe8amena. 
-    To panw aristera keli einai auto sti 8esi (0,0).
+    Prints the board of the game where with black sqaure
+    (unicode character 11035) displayed the alive cells, and
+    with white (unicode character 11036) the deads.
 
-    Paradeigma:
+    Examples:
 
     >>> game = board(5)
     >>> set_alive(game, (0,0), True)
@@ -194,7 +189,7 @@ def print_board(board):
     ⬜⬜⬜⬜⬛
     ⬜⬜⬜⬜⬛
     """ 
-    """GRAPSTE TON KWDIKA SAS EDW."""   
+
     for x in range(get_size(board)):
         s=''
         for y in range(get_size(board)):
@@ -205,16 +200,16 @@ def print_board(board):
         print(s)
 
 
-#---------------------- Askisi 6 ------------------------
+
 def neighbors(p):
-    """Geitonika kelia.
+    """Neighbor cells.
 
-    p -- 8esh keliou (tuple tis morfis (i,j)).
+    p -- position of the cell (tuple of the format (i,j)).
 
-    Epistrefei synolo (set) pou periexontai oi 8eseis twn 8 geitonikwn
-    keliwn tou p. Sto epistrefomeno synolo den periexetai to keli p.
+    Returns a set, containing the locations of neigbor cells
+     of p. The returning set does not contain cell p.
 
-    Paradeigmata:
+    Examples:
 
     >>> neighbors((3,2))
     {(3, 3), (3, 1), (2, 1), (2, 3), (4, 3), (2, 2), (4, 2), (4, 1)}
@@ -223,7 +218,7 @@ def neighbors(p):
     >>> neighbors((0,10))
     {(-1, 9), (1, 10), (-1, 11), (0, 11), (-1, 10), (1, 9), (0, 9), (1, 11)}
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     set_neighbors=set()
     i=p[0]-1
     while i<=p[0]+1:
@@ -235,18 +230,18 @@ def neighbors(p):
     set_neighbors.remove(p)
     return set_neighbors
 
-#---------------------- Askisi 7 ------------------------
+
 def place_blinker(board, p = (0,0)):
-    """Topo8etisi blinker.
+    """Blinker placement.
 
-    board -- pinakas paixnidiou.
-    p -- keli topo8etisis (tuple (i,j) me proka8orismeni timi (0,0))
+    board -- game board.
+    p -- placement cell (tuple (i,j) with default value (0,0))
 
-    Topo8etei 3 zwntanous organismous sto board 
-    se geitonika kelia tis idias stilis, arxizontas apo ti 8esi p kai
-    proxwrontas stis parakatw grammes.
+    Place 3 alive bodies to board
+    In neighbor cells of the same column, starting from position p and
+    proceeding to the bottom lines.
 
-    Paradeigmata:
+    Examples:
     
     >>> game = board(5)
     >>> place_blinker(game)
@@ -271,23 +266,23 @@ def place_blinker(board, p = (0,0)):
     ⬜⬜⬛⬜⬜
     ⬜⬜⬜⬜⬛
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     for i in range(p[0],p[0]+3):
         set_alive(board,(i,p[1]),True)
 
 
 
 def place_glider(board, p = (0,0)):
-    """Topo8etisi glider.
+    """Glider placement.
 
-    board -- pinakas paixnidiou.
-    p -- keli topo8etisis (tuple (i,j) me proka8orismeni timi (0,0))
+    board -- game board.
+    p -- placement cell (tuple (i,j) with default value (0,0))
 
-    Topo8etei 5 zwntanous organismous sto board se sximatismo glider
-    arxizontas apo tin topo8esia p, opws fainetai sta parakatw paradeigmata. 
-    Simeiwste oti to idio to keli p den einai zwntano.
+    Places 5 alive bodies to board in shapes of glider
+    starting from th elocation p, as shown in the example below.
+    Note that the same cell p is not alive.
     
-    Paradeigmata:
+    Examples:
 
     >>> game = board(7)
     >>> place_glider(game)
@@ -309,7 +304,7 @@ def place_glider(board, p = (0,0)):
     ⬜⬜⬜⬜⬛⬛⬜
     ⬜⬜⬜⬜⬜⬜⬜
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     set_alive(board,(p[0],p[1]+2),True)
     set_alive(board,(p[0]+1,p[1]),True)
     set_alive(board,(p[0]+1,p[1]+2),True)
@@ -317,16 +312,16 @@ def place_glider(board, p = (0,0)):
     set_alive(board,(p[0]+2,p[1]+2),True)
 
 
-#---------------------- Askisi 8 ------------------------
+
 def tick(board):
-    """Proxwraei to paixnidi kata ena bima stin epomeni genea.
+    """The game goes one step further to the next generation.
 
-    board -- pinakas paixnidiou.
+    board -- game board.
 
-    Allazei ton pinaka board proxwrontas mia genea, 
-    symfwna me tous kanones tou Game of Life.
+    Changes the board by passing a generation
+    ccording to the rules of Game of Life.
 
-    Paradeigma:
+    Examples:
 
     >>> game = board(6)
     >>> place_glider(game)
@@ -347,7 +342,7 @@ def tick(board):
     ⬜⬜⬜⬛⬛⬛
     ⬜⬜⬜⬜⬜⬜
     """
-    """GRAPSTE TON KWDIKA SAS EDW."""
+
     n=get_size(board)
     board2=copy_board(board)
     for k,v in board2.items():
@@ -366,12 +361,10 @@ def tick(board):
                 board[k]=True
 
 
-#---------------------- Askisi 9 ------------------------
 
 if __name__ == '__main__':
-    """Paizei to paixnidi gia mia sygkekrimeni arxiki topo8etisi, gia 
-    100 genιes. O pinakas tou paixnidiou emfanizetai se  ka8e bima.
-    Afiste toulaxiston 2 kenes grammes anamesa se diadoxikous pinakes.
+    """Plays the game for a specific initial placement for 100 generations.
+     The board of the game displays in every step.
     """
 
     # Arxikos pinakas
@@ -381,7 +374,7 @@ if __name__ == '__main__':
 
     from time import sleep
 
-    """GRAPSTE TON KWDIKA SAS APO KATW."""
+
     for x in range(1,100):
         tick(game)
         print_board(game)
